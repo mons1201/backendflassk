c93103660013ede3cdf54c112362d1382155bc7d
mons1201 <mohanmons1201@gmail.com>
2022-12-15 11:07:08 -0800

    back

--- src/join.js
+++ src/join.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 const url="http://127.0.0.1:5643"
 
+export const onTraverse=async()=>{
+    const t = await axios.get(`${url}/`)
+    return t
+}
+
+
+
 export const onCreate=async(obj)=>
 {
     //alert(JSON.stringify(obj))
