87fbcb7052046f82051b694a788e8a2a5a338756
mons1201 <mohanmons1201@gmail.com>
2022-12-15 10:20:01 -0800

    back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Create } from "./create";
 
 const App=()=>
 {
   return(
   <>
+  <Create/>
+
   </>
 
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import React from 'react';
 import App from './App';
 
 
